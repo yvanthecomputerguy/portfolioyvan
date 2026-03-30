081f9b9126daeacbd538aa7409dc12051d38d5b6
Yvan <yvanthecomputerguy@gmail.com>
2026-03-29 23:20:19 -0300

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   }
